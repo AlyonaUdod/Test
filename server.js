85f06f370696a8c397792ce7376291161beec5bf
Your Name <freelspringisoul@gmail.com>
2019-02-13 01:04:28 +0200

    22

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config()
+// require('dotenv').config()
 const path = require('path')
 const messagesRoutes = require('./routes/messagesRoutes')
 
