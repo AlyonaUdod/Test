93ed596ad699baa51dca674fa0dff8583161fb58
Your Name <freelspringisoul@gmail.com>
2019-02-13 01:42:51 +0200

    27

--- server.js
+++ server.js
@@ -1,69 +1,69 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// // require('dotenv').config()
-// const path = require('path')
-// const messagesRoutes = require('./routes/messagesRoutes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+// require('dotenv').config()
+const path = require('path')
+const messagesRoutes = require('./routes/messagesRoutes')
 
-// const PORT = process.env.PORT || 8000;
-// const app = express();
+const PORT = process.env.PORT || 8000;
+const app = express();
 
-// const favicon = require('express-favicon');
-// app.use(favicon(__dirname + 'client/public/favicon.ico'));
+const favicon = require('express-favicon');
+app.use(favicon(__dirname + 'client/public/favicon.ico'));
 
-// // app.use(express.static(path.join(__dirname)));
-// // app.use("/styles", express.static(__dirname));
-// // app.use("/images", express.static(__dirname + '/images'));
-// // app.use("/scripts", express.static(__dirname + '/scripts'));
-// // app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname)));
+// app.use("/styles", express.static(__dirname));
+// app.use("/images", express.static(__dirname + '/images'));
+// app.use("/scripts", express.static(__dirname + '/scripts'));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
-// app.get('/', (req, res) => {
-//   res.send('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-//   // res.sendFile(path.join(__dirname + '/client/build/index.html'))
+app.get('/', (req, res) => {
+  res.send('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+// mongoose.Promise = global.Promise;
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.connect(process.env.MONGODB_URL)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use(cors());
+// app.options('*', cors());
+
+// app.use('/api/messages', messagesRoutes);
+
+// app.use((req, res, next) => {
+//   res
+//     .status(404)
+//     .json({err: '404'});
+// });
+
+// app.use((err, req, res, next) => {
+//   res
+//     .status(500)
+//     .json({err: '500'});
 // })
 
-// // mongoose.Promise = global.Promise;
-// // mongoose.set('useNewUrlParser', true);
-// // mongoose.set('useFindAndModify', false);
-// // mongoose.set('useCreateIndex', true);
-// // mongoose.connect(process.env.MONGODB_URL)
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// // app.use(cors());
-// // app.options('*', cors());
-
-// // app.use('/api/messages', messagesRoutes);
-
-// // app.use((req, res, next) => {
-// //   res
-// //     .status(404)
-// //     .json({err: '404'});
-// // });
-
-// // app.use((err, req, res, next) => {
-// //   res
-// //     .status(500)
-// //     .json({err: '500'});
-// // })
-
-// app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
 
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8000;
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 8000;
 
-app.get('/', (request, res) => {
-    res.send('Conneccteedd!!')
-    }
-)
+// app.get('/', (request, res) => {
+//     res.send('Conneccteedd!!')
+//     }
+// )
 
-app.get('/something', (request, res) => {
-    res.send('somethingg!!')
-    }
-)
+// app.get('/something', (request, res) => {
+//     res.send('somethingg!!')
+//     }
+// )
 
-app.listen(PORT, () => console.log('Example app listening on post 3000!'))
+// app.listen(PORT, () => console.log('Example app listening on post 3000!'))
