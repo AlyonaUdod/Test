d1482af70720a9c06b8a2e6f16d27d62a376bfc2
Your Name <freelspringisoul@gmail.com>
2019-02-13 02:25:38 +0200

    28

--- server.js
+++ server.js
@@ -34,17 +34,17 @@ app.use('/api/messages', messagesRoutes);
 // app.use(cors());
 // app.options('*', cors());
 
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({err: '404'});
-});
+// app.use((req, res, next) => {
+//   res
+//     .status(404)
+//     .json({err: '404'});
+// });
 
-app.use((err, req, res, next) => {
-  res
-    .status(500)
-    .json({err: '500'});
-})
+// app.use((err, req, res, next) => {
+//   res
+//     .status(500)
+//     .json({err: '500'});
+// })
 
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
 
