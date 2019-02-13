0da75e5a7fe1542c01217d09c5b32447594fc7f0
Your Name <freelspringisoul@gmail.com>
2019-02-13 09:11:01 +0200

    28

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
 // mongoose.connect('mongodb://admin:qwertyui90@ds157064.mlab.com:57064/sandbox_test', function(err) {
 //     if (err)
 //         return console.error(err);
 // });
-const cors = require('cors');
+// const cors = require('cors');
 // require('dotenv').config()
 // const path = require('path')
 // const messagesRoutes = require('./routes/messagesRoutes')
